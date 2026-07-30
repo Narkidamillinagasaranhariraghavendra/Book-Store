cf172432b3d2141ec348213d5f78defbc930b8a9
sushma-23s <sushmanarkidamill@gmail.com>
2026-07-30 23:54:48 +0530

    project donee

--- Frontend/src/pages/DeleteBook.jsx
+++ Frontend/src/pages/DeleteBook.jsx
@@ -9,19 +9,20 @@ const DeleteBook = () => {
   const [loading, setLoading] = React.useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
-  const {enqueueSnackbar} = useSnackbar();
+  const { enqueueSnackbar } = useSnackbar();
+
   const handleDeleteBook = () => {
     setLoading(true);
+
     axios.delete(`http://localhost:5555/books/${id}`)
       .then((response) => {
         setLoading(false);
-        enqueueSnackbar('Book deleted successfully',{variant:'success'});
+        enqueueSnackbar('Book deleted successfully', { variant: 'success' });
         navigate('/');
       })
       .catch((error) => {
         setLoading(false);
-        //alert('An error happened. Please check console');
-        enqueueSnackbar('Error',{variant:'error'});
+        enqueueSnackbar('Error', { variant: 'error' });
         console.log(error);
       });
   };
@@ -30,18 +31,23 @@ const DeleteBook = () => {
     <div>
       <BackButton />
       <h1 className="text-3xl my-4">Delete Book</h1>
+
       {loading ? <Spinner /> : ''}
+
       <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
-        <h3 className='text-2xl text-gray-500'>Are you sure you want to delete this book?</h3>
-        <button className='p-4 bg-red-600 text-white m-8 full'
-          onClick={handleDeleteBook}>
-            Yes,I want to delete this book
+        <h3 className='text-2xl text-gray-500'>
+          Are you sure you want to delete this book?
+        </h3>
 
-
-     </button>
+        <button
+          className='p-4 bg-red-600 text-white m-8 full'
+          onClick={handleDeleteBook}
+        >
+          Yes, I want to delete this book
+        </button>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default DeleteBook
+export default DeleteBook; 
